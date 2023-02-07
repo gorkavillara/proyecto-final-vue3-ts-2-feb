c5e26ed63cdab2cbaa32f1902f253ecb3d228c9b
gorkavillar <gorka@gorkavillar.com>
2023-02-07 12:32:34 +0100

    🖌️ Definiendo estilos dashard

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Dashboard />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+<script lang="ts" setup>
+  import Dashboard from './views/Dashboard.vue';
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
+html {
+  height: 100vh;
+  width: 100vw;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="dashboard">
+        <div class="sidebar">
+            <div class="card">
+                <div style="font-weight: 800">Logo</div>
+                <div>Secciones</div>
+                <div>Log out</div>
+            </div>
+        </div>
+        <div class="content">
+            Hola mundo
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+    div.dashboard {
+        width: 100%;
+        height: 100%;
+        min-height: 100vh;
+        background-color: #eefff9;
+        display: flex;
+    }
+
+    div.sidebar {
+        width: 30%;
+        padding: 2rem;
+        box-sizing: border-box;
+    }
+    div.sidebar .card {
+        background-color: white;
+        height: 100%;
+        border-radius: 1.5rem;
+        box-shadow: 0 0 30px rgba(88, 112, 121, 0.158);
+        padding: 2rem 0.5rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    div.content {
+        margin-top: 2rem;
+    }
+</style>
